4840590de9a876d0fc0746c769106a6a88cd9afe
Randi Nash <randidena@live.com>
2017-09-21 23:46:50 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,5 +1,7 @@
-levitationForce = 0
-until levitationForce == 10
-  puts "Wingardium Leviosa"
-  levitationForce += 1
+def using_until
+  levitationForce = 0
+  until levitationForce == 10
+    puts "Wingardium Leviosa"
+    levitationForce += 1
+  end
 end
