949d111d164e43aa3c4a157f3843b62f071e6175
Randi Nash <randidena@live.com>
2017-09-21 23:47:16 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,5 +1,7 @@
-levitationForce = 0
-while levitationForce < 10
-  puts "Wingardium Leviosa"
-  levitationForce += 1
+def using_while
+  levitationForce = 0
+  while levitationForce < 10
+    puts "Wingardium Leviosa"
+    levitationForce += 1
+  end
 end
